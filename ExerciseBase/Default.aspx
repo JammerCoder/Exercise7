b954e20aa81e5eab8f41031cf1c8f43c67b3f1b5
JammerCoder <rolandbarro@gmail.com>
2016-04-04 19:11:55 +0800

    Exercise 7
    PinkTech Part1 Exercise 7 for Reporting v1

---
+++ ExerciseBase/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ExerciseBase.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Exercise 7</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table width="400px" align="center">
+                <tr valign="top">
+                    <td align="left">
+                        <asp:Button ID="btnClickMe" runat="server" Text="ClickMe!" ToolTip="Click for Magic!" OnClick="btnClickMe_Click" />     
+                    </td>
+                    <td align="left">                        
+                        <asp:Label ID="lblDisplay" runat="server" Text="" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Literal ID="litDisplay" runat="server" Text="Test" />
+                    </td>
+                </tr>
+        </table>            
+    </div>
+    </form>
+</body>
+</html>
---
+++ ExerciseBase/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExerciseBase
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //if (!IsPostBack)
+                //{
+                //    this.lblPostBack.Text = "First Visit!";
+                //}
+                //else
+                //{
+                //    this.lblPostBack.Text = "Postback had just happen!";
+                //}                    
+
+                ViewState["TestValue"] = "RockAndRoll";
+
+            }
+            catch(Exception ex)
+            {
+                this.litDisplay.Text = ex.Message;
+            }
+        }
+
+        protected void btnClickMe_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.litDisplay.Text = ViewState["TestValue"].ToString();
+                ViewState["TestValue"] = "<b>Country&Western</b>";
+                this.lblDisplay.Text = ViewState["TestValue"].ToString();
+            }
+            catch (Exception ex)
+            {
+                this.litDisplay.Text = ex.Message;
+            }
+
+            
+        }
+    }
+}
